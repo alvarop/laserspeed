05a9f38cbdb4e2bcb6adfa009c6accd62fa8462c
Alvaro Prieto <apg88zx@gmail.com>
2013-05-18 20:52:46 -0700

    pulse-width testing seems to work :-)

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 // Basic LED-blink for the MBED.
 //
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include "LPC17xx.h"
 #include "lcd.h"
 
@@ -17,7 +19,22 @@
 #define BUTTON1     (1 << 0) 
 #define BUTTON2     (1 << 1)
 
+typedef enum {
+  IDLE,
+  ALIGN,
+  PULSE,
+  FREQUENCY
+} state_t;
+
 volatile uint32_t systick_counter = 0;
+
+volatile state_t state = IDLE;
+
+uint32_t pulse_width = 100000000;
+
+char lcd_buf[32];
+
+void pwm_init(uint32_t period, uint32_t pulse);
 
 void SysTick_Handler (void) {
   systick_counter++;
@@ -60,17 +77,23 @@ void process_buttons() {
 
   if(last_buttons != buttons) {
     last_buttons = buttons;
-    lcd_putc(0xFE); lcd_putc(0x1);
 
     if(buttons & BUTTON1) {
-      lcd_puts("start");
+      lcd_putc(0xFE); lcd_putc(0x1);
+      lcd_puts("align");
+      state = ALIGN;
     } else if(buttons & BUTTON2) {
-      lcd_puts("stop");
+      pulse_width = 100000000;
+      lcd_putc(0xFE);
+      lcd_putc(0x1);
+      sprintf(lcd_buf, "%d us", pulse_width / 100);
+      pwm_init(pulse_width * 2, pulse_width);
+      state = PULSE;
     }
   }
 }
 
-void pwm_init() {
+void pwm_init(uint32_t period, uint32_t pulse) {
 
   // PWM1 reset
   LPC_PWM1->TCR = (1 << 1);
@@ -87,15 +110,15 @@ void pwm_init() {
   // No pull down on GPIO 2.0
   LPC_PINCON->PINMODE4 |= (2 << 0);
 
-  // Reset on MR0
-  LPC_PWM1->MCR = (1 << 1);
+  // Stop on MR0
+  LPC_PWM1->MCR = (1 << 2);
 
   // PWM1 output enabled
   LPC_PWM1->PCR = (1 << 9);
 
   // 50% duty cycle, 1 second period
-  LPC_PWM1->MR0 = 100000000;
-  LPC_PWM1->MR1 = 50000000;
+  LPC_PWM1->MR0 = period;
+  LPC_PWM1->MR1 = pulse;
 
   // M0 and M1 latch
   LPC_PWM1->LER = (1 << 0) | (1 << 1);
@@ -103,6 +126,8 @@ void pwm_init() {
   // Enable PWM mode
   LPC_PWM1->TCR = (1 << 0) | (1 << 3);
 }
+
+static volatile uint8_t pulse_received = 0;
 
 int main() {
   
@@ -122,6 +147,7 @@ int main() {
 
   // Laser control output
   LPC_GPIO2->FIODIR |= (1 << LASER_PIN);
+  LPC_GPIO2->FIOSET |= (1 << 0);
 
   lcd_init();
 
@@ -131,11 +157,17 @@ int main() {
 
   lcd_puts("LaserSpeed v0.1");
 
-  pwm_init();
+  // Enable GPIO 2.0 interrupt
+  LPC_GPIOINT->IO0IntClr |= (1 << DETECT_PIN);
+  LPC_GPIOINT->IO0IntEnR |= (1 << DETECT_PIN);
+  NVIC_EnableIRQ(EINT3_IRQn);
 
   delay_ms(1000);
 
   for(;;) {
+
+    memset(lcd_buf, ' ', sizeof(lcd_buf));
+
     if(0 == (systick_counter & 0x1FF)) {
       // Toggle LED
       LPC_GPIO1->FIOPIN ^= (1 << LED4_PIN);
@@ -144,10 +176,65 @@ int main() {
     // Button processing
     process_buttons();
 
-    if(LPC_GPIO0->FIOPIN & (1 << DETECT_PIN)) {
-      // detect!
+    switch(state) {
+      case IDLE: 
+        // Do nothing
+        break;
+
+      case ALIGN: 
+        // Set pin as GPIO
+        LPC_PINCON->PINSEL4 &= ~(1 << 0);
+        LPC_GPIO2->FIOCLR |= (1 << 0);
+
+        if(LPC_GPIO0->FIOPIN & (1 << DETECT_PIN)) {
+
+          // Clear display
+          lcd_putc(0xFE);
+          lcd_putc(0x1);
+
+          lcd_puts("aligned!");
+
+          delay_ms(500);
+          LPC_GPIO2->FIOSET |= (1 << 0);
+
+          state = IDLE;
+        }
+
+        break;
+
+      case PULSE:
+        // If pulse is detected, half the pulse width and try again
+        if(pulse_received) {
+          pulse_received = 0;
+          
+          lcd_putc(0xFE);
+          lcd_putc(0x1);
+          sprintf(lcd_buf, "%d us", pulse_width / 100);
+          lcd_puts(lcd_buf);
+
+          pulse_width >>= 1;
+          
+          // Turn off LED
+          LPC_PINCON->PINSEL4 &= ~(1 << 0);
+          LPC_GPIO2->FIOSET |= (1 << 0);
+          
+          delay_ms(500);
+          pwm_init(pulse_width * 2, pulse_width);
+        }
+
+        //state = IDLE;
+        break;
+
+      case FREQUENCY: 
+
+        break;
     }
 
     __WFI();      
   }
+}
+
+void EINT3_IRQHandler(void) {
+  LPC_GPIOINT->IO0IntClr |= (1 << DETECT_PIN);
+  pulse_received = 1;
 }
